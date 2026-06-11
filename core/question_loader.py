ecb88dc3f721d78430b456efebf0f359fa1e2f06
SergeiKirillov <sergei.kirillov.a@gmail.com>
2026-06-11 21:22:06 +0500

    eng 11.06.2026 21:17 Linux

--- core/question_loader.py
+++ core/question_loader.py
@@ -3,13 +3,21 @@ import os
 
 from core.question import Question
 from core.answer import Answer
+from pathlib import Path
 
 class QuestionLoader:
     @staticmethod
     def load(filename):
         base_dir = os.path.dirname(os.path.abspath(__file__))
         file_path = os.path.join(base_dir, filename)
-        with open(filename, "r",encoding="utf-8") as f:
+
+        #data/questions.json"
+        file_path = Path("data")/"questions.json"
+        # Создаем директорию, если она не существует
+        #file_path.parent.mkdir(parents=True, exist_ok=True)
+        
+        # with open(filename, "r",encoding="utf-8") as f: #Windows
+        with open(file_path, "r",encoding="utf-8") as f:
             data = json.load(f)
             
             """
