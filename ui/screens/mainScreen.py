73fef339d6aa3713274b4e00bb61e447cff3fe65
SergeiKirillov <sergei.kirillov.a@gmail.com>
2026-07-04 17:39:47 +0500

    04.07.2026 17:39

--- ui/screens/mainScreen.py
+++ ui/screens/mainScreen.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import ScreenManager, Screen
 from ui.screens.setting import Setting
@@ -14,19 +16,39 @@ class MainScreen(BaseScreen):
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        
-        blMain = BoxLayout(orientation="vertical")
+        blMain = BoxLayout(orientation="vertical", padding=dp(10), spacing=dp(10))
+        blMainTitle = BoxLayout(orientation="horizontal", padding=dp(10), spacing=dp(10))
+        blMainContent = BoxLayout(orientation="horizontal", padding=dp(10), spacing=dp(10))
+        blMainMenu = BoxLayout(orientation="horizontal", padding=dp(10), spacing=dp(10))
+        blMainLeft = BoxLayout(orientation="vertical", padding=dp(10), spacing=dp(10), size_hint=(None, 1), width=dp(200))
+        blMainRight = BoxLayout(orientation="vertical", padding=dp(10), spacing=dp(10),size_hint=(None, 1), width=dp(200))
+        blMainCenter = BoxLayout(orientation="vertical", padding=dp(10), spacing=dp(10))
 
-        lblTitle=Label(text="Главный экрaн", color="yellow")
-        lblTitle.font_size=Constants.HEADER_HEIGHT*0.5
+        blMainTitle.add_widget(Label(text="Главный экран", color="yellow", font_size=Constants.HEADER_HEIGHT*0.5))
+        txtLoginName=TextInput(text="Введите табельный номер, и нажмите Enter", size_hint=(None, None), size=(500, 30))
+        #TODO: добавить обработку нажатия Enter в TextInput
+        #btnLoginName=Button(text="Войти",size_hint=(None, None),size=(200,25))    
+        blMainCenter.add_widget(Widget())  # Добавляем пустой виджет для отступа
+        blMainCenter.add_widget(txtLoginName)
+        #blMainCenter.add_widget(btnLoginName)
+        blMainCenter.add_widget(Widget())  # Добавляем пустой виджет для отступа
+        blMainContent.add_widget(blMainLeft)
+        blMainContent.add_widget(blMainCenter)
+        blMainContent.add_widget(blMainRight)
+        blMainMenu.add_widget(navigatorMenu(self.change_screen))
+        blMain.add_widget(blMainTitle)
+        blMain.add_widget(blMainContent)
+        blMain.add_widget(blMainMenu)
+
 
         #btnTheme=Button(text="Выбор темы заданий",size_hint=(None, None),size=(200,20))
         #btnTheme.id="btn_select_theme"
         #btnTheme.bind(on_release=self.btn_click)
 
-        blMain.add_widget(lblTitle)
+        #blMainTitle.add_widget(lblTitle)
+        #blMain.add_widget(blMainTitle)
         #blMain.add_widget(btnTheme)
-        blMain.add_widget(navigatorMenu(self.change_screen))
+        #blMain.add_widget(navigatorMenu(self.change_screen))
 
         self.session.user="019261"
 
